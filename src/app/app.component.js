ded98bb466d34c4a4ae5b1341250786b13445679
Chandler Redding <chandler@devtub.com>
2017-01-15 04:01:18 -0700

    ...

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -95,48 +95,50 @@ function keyup(event) {
 }
 
 function render() {
-    ctx.clearRect(0, 0, width, height);
-    ctx.fillStyle = '#0f0';
-    ctx.lineWidth = 0;
-    renderData.snake.forEach(val => {
-        let x = PIXEL_SIZE * val[0];
-        let y = PIXEL_SIZE * val[1];
+    window.requestAnimationFrame(() => {
+        ctx.clearRect(0, 0, width, height);
+        ctx.fillStyle = '#0f0';
+        ctx.lineWidth = 0;
+        renderData.snake.forEach(val => {
+            let x = PIXEL_SIZE * val[0];
+            let y = PIXEL_SIZE * val[1];
+            ctx.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
+        });
+        ctx.fillStyle = '#f00';
+        let x = PIXEL_SIZE * renderData.food[0];
+        let y = PIXEL_SIZE * renderData.food[1];
         ctx.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
-    });
-    ctx.fillStyle = '#f00';
-    let x = PIXEL_SIZE * renderData.food[0];
-    let y = PIXEL_SIZE * renderData.food[1];
-    ctx.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
-    ctx.fillStyle = 'rgba(255, 0, 0, 0.7)';
-    ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)';
-    ctx.lineWidth = 10;
-    ctx.font = 'bold 300px sans-serif';
-    ctx.textAlign = 'center';
-    switch (renderData.mode) {
-        case '3':
-            ctx.strokeText('3', centerX, centerY + 50);
-            ctx.fillText('3', centerX, centerY + 50);
-            break;
-        case '2':
-            ctx.strokeText('2', centerX, centerY + 50);
-            ctx.fillText('2', centerX, centerY + 50);
-            break;
-        case '1':
-            ctx.strokeText('1', centerX, centerY + 50);
-            ctx.fillText('1', centerX, centerY + 50);
-            break;
-        case 'play':
-            break;
-        case 'end':
-            ctx.font = 'bold 120px sans-serif';
-            ctx.strokeText('Game Over', centerX, centerY + 10);
-            ctx.fillText('Game Over', centerX, centerY + 10);
-            break;
-    }
-    canvas.toBlob(blob => {
-        URL.revokeObjectURL(data.render);
-        let url = URL.createObjectURL(blob);
-        data.render = URL.createObjectURL(blob);
+        ctx.fillStyle = 'rgba(255, 0, 0, 0.7)';
+        ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)';
+        ctx.lineWidth = 10;
+        ctx.font = 'bold 300px sans-serif';
+        ctx.textAlign = 'center';
+        switch (renderData.mode) {
+            case '3':
+                ctx.strokeText('3', centerX, centerY + 50);
+                ctx.fillText('3', centerX, centerY + 50);
+                break;
+            case '2':
+                ctx.strokeText('2', centerX, centerY + 50);
+                ctx.fillText('2', centerX, centerY + 50);
+                break;
+            case '1':
+                ctx.strokeText('1', centerX, centerY + 50);
+                ctx.fillText('1', centerX, centerY + 50);
+                break;
+            case 'play':
+                break;
+            case 'end':
+                ctx.font = 'bold 120px sans-serif';
+                ctx.strokeText('Game Over', centerX, centerY + 10);
+                ctx.fillText('Game Over', centerX, centerY + 10);
+                break;
+        }
+        canvas.toBlob(blob => {
+            URL.revokeObjectURL(data.render);
+            let url = URL.createObjectURL(blob);
+            data.render = URL.createObjectURL(blob);
+        });
     });
 }
 
